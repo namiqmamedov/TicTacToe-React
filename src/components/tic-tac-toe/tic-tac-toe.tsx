bae1f536c4b420b187da6181f9e2a719b3ddb98f
namiqmamedov <namigmm@code.edu.az>
2023-07-05 02:41:06 +0400

    Updated tic tac toe component

--- src/components/tic-tac-toe/tic-tac-toe.tsx
+++ src/components/tic-tac-toe/tic-tac-toe.tsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react'
 import { Board } from './Board'
+import './tic-tac-toe.sass'
+
+
+type BoardArray = Array<Array<string | null>>
 
 export const TicTacToe = () => {
-  const [board,setBoard] = useState<any>(
+  const [board,setBoard] = useState<BoardArray>(
     Array.from(
       {
       length:3
       },
       () => Array.from({length:3}, () =>  null)) // 3 rows & 3 columns
   );
+
+  const [player,setPlayer] = useState<string>('X')
+  const[winner,setWinner] = useState<string | null>(null)
+
+      const handleOnClick = (row: number, col: number) => {
+          if(board[row][col] || winner) {
+              return;
+          }
+
+          const updatePlayerBoard = board.map((newRow,rowIndex) => newRow.map((cell,cellIndex) =>
+           rowIndex === row && cellIndex === col ? player : cell))
+      }
+
   return (
     <div className='game'>
         <h1>Tic-Tac-Toe</h1>
-        <Board board={board} handleClick={() => ""}/>
+        <Board board={board} handleClick={() => handleOnClick}/>
     </div>
   )
 }
